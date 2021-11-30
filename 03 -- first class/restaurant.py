892f78e323bd60a79e4667dd5fa20471944317bf
backendprincess <lemur.king.julien@gmail.com>
2021-11-30 21:31:55 +0200

    Homework: Restaurant class updates

--- 03 -- first class/restaurant.py
+++ 03 -- first class/restaurant.py
@@ -2,6 +2,7 @@ class Restaurant():
 	def __init__(self, restaurant_name, cuisine_type):
 		self.restaurant_name = restaurant_name
 		self.cuisine_type = cuisine_type
+		self.number_served = 0
 		
 	def describe_restaurant(self):
 		print("The restaurant '" + self.restaurant_name + 
@@ -11,11 +12,20 @@ class Restaurant():
 		print("The restaurant '" + self.restaurant_name + 
 			"' is open for business!")
 			
+	def set_number_served(self, number_served):
+		self.number_served = number_served
+	
+	def increment_number_served(self, customers):
+		self.number_served += customers
+			
 my_restaurant = Restaurant("McDonalds", "American")
 my_other_restaurant = Restaurant("Celentano", "Italian")
 
 my_restaurant.describe_restaurant();
 my_restaurant.open_restaurant();
+my_restaurant.set_number_served(100)
+print(my_restaurant.restaurant_name + " served " + str(my_restaurant.number_served) + " clients!")
+my_restaurant.increment_number_served(213)
+print(my_restaurant.restaurant_name + " served " + str(my_restaurant.number_served) + " clients!")
 
-my_other_restaurant.describe_restaurant();
-my_other_restaurant.open_restaurant();
+
