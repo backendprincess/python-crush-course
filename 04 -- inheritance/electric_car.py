ebf1a120d911e277b8239064f882a65ae0609f2f
backendprincess <lemur.king.julien@gmail.com>
2021-12-01 17:54:28 +0200

    Adding instance as attribute

--- 04 -- inheritance/electric_car.py
+++ 04 -- inheritance/electric_car.py
@@ -29,17 +29,20 @@ class ElectricCar(Car):
 	def __init__(self, make, model, year):
 		super().__init__(make, model, year)
 		"""Attribute that doesn't exist in the parent"""
-		self.battery_size = 70
-		
-	"""Method that doesn't exist in the parent"""
-	def describe_battery(self):
-		print("This car has a " + str(self.battery_size) + "kWh battery.")
+		self.battery = Battery()
 	
 	"""Overriding a parent method"""
 	def fill_gas_tank(self):
 		print("This car doesn't have a gas tank!")
+
+class Battery():
+	def __init__(self, battery_size=70):
+		self.battery_size = battery_size
+		
+	def describe_battery(self):
+		print("This is a " + str(self.battery_size) + "kWh battery.")
 		
 my_tesla = ElectricCar('tesla', 'model s', 2016)
 print(my_tesla.get_descriptive_name())
-my_tesla.describe_battery()
+my_tesla.battery.describe_battery()
 my_tesla.fill_gas_tank();
