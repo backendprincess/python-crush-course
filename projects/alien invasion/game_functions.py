19378b5fdde09cd99aea4269916a6a44121816cb
backendprincess <lemur.king.julien@gmail.com>
2021-12-06 15:29:56 +0200

    Add Play button

--- projects/alien invasion/game_functions.py
+++ projects/alien invasion/game_functions.py
@@ -4,7 +4,7 @@ from bullet import Bullet
 from alien import Alien
 from time import sleep
 
-def check_events(ai_settings, screen, ship, bullets):
+def check_events(ai_settings, screen, stats, play_button, ship, bullets):
 	"""Respond to keypresses and mouse events."""
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
@@ -14,8 +14,11 @@ def check_events(ai_settings, screen, ship, bullets):
 			check_keydown_events(event, ai_settings, screen, ship, bullets)
 		elif event.type == pygame.KEYUP:
 			check_keyup_events(event, ship)
+		elif event.type == pygame.MOUSEBUTTONDOWN:
+			mouse_x, mouse_y = pygame.mouse.get_pos()
+			check_play_button(stats, play_button, mouse_x, mouse_y)
 
-def update_screen(ai_settings, screen, ship, aliens, bullets):
+def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button):
 	"""Update images on the screen and flip to the new screen."""
 	# Redraw the screen during each pass through the loop.
 	screen.fill(ai_settings.bg_color)
@@ -25,6 +28,10 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
 		bullet.draw_bullet()
 	ship.blitme()
 	aliens.draw(screen)
+	
+	# Draw the play putton if the game is inactive.
+	if not stats.game_active:
+		play_button.draw_button()
 	
 	# Make the most recently drawn screen visible.
 	pygame.display.flip()
@@ -168,3 +175,8 @@ def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
 			# Treat this the same as if the ship got hit.
 			ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
 			break
+			
+def check_play_button(stats, play_button, mouse_x, mouse_y):
+	"""Start a new game when player clicks Play."""
+	if play_button.rect.collidepoint(mouse_x, mouse_y):
+		stats.game_active = True
